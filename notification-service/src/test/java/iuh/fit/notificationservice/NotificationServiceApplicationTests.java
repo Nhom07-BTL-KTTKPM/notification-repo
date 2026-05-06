c4e959c42d9ef86633c7c8f4e67d02b991711a71
Nguyen Quoc Huy <122158094+Nine99Huy8181@users.noreply.github.com>
2026-05-06 14:28:59 +0700

    feat: add variable env, delete value in application

--- notification-service/src/test/java/iuh/fit/notificationservice/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/iuh/fit/notificationservice/NotificationServiceApplicationTests.java
@@ -2,9 +2,17 @@ package iuh.fit.notificationservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class NotificationServiceApplicationTests {
+
+    @MockitoBean
+    private RabbitTemplate rabbitTemplate;
 
     @Test
     void contextLoads() {
