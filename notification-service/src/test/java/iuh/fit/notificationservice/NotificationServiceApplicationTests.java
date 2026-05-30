f655c974b528d3898104fb60f2a5e55b89cedef8
Nguyen Quoc Huy <122158094+Nine99Huy8181@users.noreply.github.com>
2026-05-30 15:11:27 +0700

    chore: setup production deploy config

--- notification-service/src/test/java/iuh/fit/notificationservice/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/iuh/fit/notificationservice/NotificationServiceApplicationTests.java
@@ -3,7 +3,12 @@ package iuh.fit.notificationservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.cloud.discovery.enabled=false",
+    "eureka.client.enabled=false",
+    "eureka.client.register-with-eureka=false",
+    "eureka.client.fetch-registry=false"
+})
 class NotificationServiceApplicationTests {
 
     @Test
